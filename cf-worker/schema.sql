c25c110b7ac42309c6ebf2c79115ab04c4afba94
Dhafer84 <boutheljad84@gmail.com>
2026-03-03 11:42:50 +0100

    add backend

--- cf-worker/schema.sql
+++ cf-worker/schema.sql
@@ -1,13 +1,9 @@
 DROP TABLE IF EXISTS projects;
 
-CREATE TABLE projects (
+CREATE TABLE IF NOT EXISTS projects (
   id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  client TEXT NOT NULL,
-  payload TEXT NOT NULL,
-  created_at TEXT NOT NULL,
-  updated_at TEXT NOT NULL
+  label TEXT,
+  meta_json TEXT NOT NULL,
+  criteria_json TEXT NOT NULL,
+  saved_at TEXT NOT NULL
 );
-
-CREATE UNIQUE INDEX idx_projects_name_client
-ON projects(name, client);
